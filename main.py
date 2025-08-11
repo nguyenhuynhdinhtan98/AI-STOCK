78e60bff1e5ef3f0a1a7104ec27f3ac92749646a
Tân Nguyễn <nguyenhuynhdinhtan98@gmail.com>
2025-08-11 20:46:38 +0700

    demo

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 from vnstock import Vnstock
-## Bạn cũng có thể gọi các class trong theo vị trí chi tiết của nguồn dữ liệu
-from vnstock.explorer.vci import Listing, Quote, Company, Finance, Trading
+from google import genai
 
-# hoặc
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key="AIzaSyBh4yjR8V6ZNNUFsS-d_m3A9JWIKB__0n4")
 
-from vnstock.explorer.tcbs import Quote, Company, Finance, Trading, Screener
+
+
 stock = Vnstock().stock(symbol='ACB', source='VCI')
-stockvci = stock.listing.all_symbols()
-stock.listing.symbols_by_exchange()
-print(stockvci)
+a = stock.quote.history(start='2025-01-01', end='2025-08-11', interval='1D')
+print(a)
+
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=f'Gia trung binh 10 ngay hien tai {a.to_string()}'
+)
+print(response.text)
